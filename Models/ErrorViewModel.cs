9156542e183d93927438082a0a6273737acc959f
Anjali-st <anjali90908080@gmail.com>
2023-09-11 00:38:10 +0530

    Using Custom Model Binding

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sec_04UsingCustomModelBinding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
